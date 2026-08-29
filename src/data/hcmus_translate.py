03970caf41d81f0685e17cc9befbf16127aa2997
JuzHarii <lethanhhai177@gmail.com>
2026-08-29 13:48:41 +0700

    feat: implement HCMUS Sino-Nom transliteration script and update project dependencies

--- src/data/hcmus_translate.py
+++ src/data/hcmus_translate.py
@@ -3,6 +3,7 @@ import json
 import time
 import sys
 import io
+from tqdm import tqdm
 
 def hcmus_translate(text):
     url = 'https://tools.clc.hcmus.edu.vn/api/web/clc-sinonom/sinonom-transliteration'
@@ -20,12 +21,31 @@ def hcmus_translate(text):
         print(f'[ERR] "{text}": {response.text}')
         return 'Cannot translate this text.'
 
-# Đoạn code dưới đây sẽ chỉ chạy khi bạn chạy trực tiếp file này
 if __name__ == "__main__":
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    # # Test thử với một từ Hán Nôm
+    # sample_text = "是日文曉有德并二營属将走出真福安塲"
+    # print(f"Đang dịch: {sample_text}")
+    # translated = hcmus_translate(sample_text)
+    # print(f"Kết quả: {translated}")
+
+    input_path = 'data/result/nom_only.txt'
+    output_path = 'data/result/comparison.txt'
     
-    # Test thử với một từ Hán Nôm
-    sample_text = "是日文曉有德并二營属将走出真福安塲"
-    print(f"Đang dịch: {sample_text}")
-    translated = hcmus_translate(sample_text)
-    print(f"Kết quả: {translated}")
+    #Dem so dong
+    with open(input_path, 'r', encoding='utf-8') as f:
+        total_lines = sum(1 for line in f if line.strip())
+
+    with open(input_path, 'r', encoding='utf-8') as fin, \
+         open(output_path, 'a', encoding='utf-8') as fout:
+        fout.write("Nom, Vietnamese translated by hcmus\n")
+        
+        with tqdm(total=total_lines, desc="Translating Nom to Vietnamese", unit="line") as pbar:
+            for line in fin:
+                text = line.strip()
+                if not text:
+                    continue
+                translated_text = hcmus_translate(text)
+                fout.write(f"{text},{translated_text}\n")
+                pbar.update(1)
+
